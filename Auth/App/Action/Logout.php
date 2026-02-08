1b4102eefa5436961d701876bef2b31b0925b7f9
Semen007 <semenchipicov@gmail.coL>
2026-02-08 19:29:34 +0300

    fix

--- Auth/App/Action/Logout.php
+++ Auth/App/Action/Logout.php
@@ -1,10 +1,21 @@
 <?php
 namespace Auth\App\Action;
 
+use Auth\App\Service\Auth;
+use Auth\Sys\Response;
+
 class Logout extends _Base
 {
 	public function __invoke()
 	{
+        if (empty($_POST)) {
+            throw new \Exception('Только для POST запросов');
+        }
 
+        if (Auth::isAuthorized()) {
+            Auth::logout();
+        }
+
+        Response::redirect('http://authorization/');
 	}
 }
