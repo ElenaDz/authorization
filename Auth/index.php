df2fea65ab04d17c34bafbcad56a7cb6320214f8
ChakoRus <chalenkoa@yandex.ru>
2026-03-12 20:12:57 +0300

    bench

--- Auth/index.php
+++ Auth/index.php
@@ -1,6 +1,10 @@
 <?php
+$start = microtime(true);
+
 require __DIR__.'/autoload.php';
 
 \Auth\Sys\Error::init();
 
 \Auth\Sys\Routing::run();
+
+echo round((microtime(true) - $start)*1000).' msec';
