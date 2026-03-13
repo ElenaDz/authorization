b5bd30bf42b38bab0920999e66ed71f351d44891
ChakoRus <chalenkoa@yandex.ru>
2026-03-13 11:23:50 +0300

    Update Request.php

--- Auth/Sys/Request.php
+++ Auth/Sys/Request.php
@@ -7,4 +7,28 @@ class Request
 	{
 		return file_exists(__DIR__.'/../_development');
 	}
+
+	public static function getIpRemote()
+	{
+		$remote_addr = null;
+
+		if (@$_SERVER['HTTP_X_FORWARDED_FOR'])
+		{
+			$remote_addr = (
+				( strpos(@$_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false )
+				? explode(',', @$_SERVER['HTTP_X_FORWARDED_FOR'])[0]
+				: @$_SERVER['HTTP_X_FORWARDED_FOR']
+			);
+
+		} elseif (@$_SERVER["HTTP_CF_CONNECTING_IP"])
+		{
+			$remote_addr = @$_SERVER["HTTP_CF_CONNECTING_IP"];
+		}
+
+		if ($remote_addr) {
+			@$_SERVER['REMOTE_ADDR'] = $remote_addr;
+		}
+
+		return @$_SERVER['REMOTE_ADDR'];
+	}
 }
